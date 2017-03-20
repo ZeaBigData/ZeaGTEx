7da88a2feadf368e014416c04de0a0bd1e78e515
yangjl0930@gmail.com <yangjl0930@gmail.com>
2017-03-20 16:41:32 -0700

    updates

--- profiling/2.Walley_etal/2.B.1_count_exon.R
+++ profiling/2.Walley_etal/2.B.1_count_exon.R
@@ -12,7 +12,9 @@ source("lib/run_kallisto.R")
 
 files <- list.files(path="~/dbcenter/zeabigdata", pattern="fastq$", full.names = TRUE)
 #c('submission','study','sample','experiment')
-df <- data.frame(fq=files, outdir=paste0("largedata/walley/", gsub(".*\\/|.sra.fastq", "", files)))
+df <- data.frame(fq=files, outdir= paste0("largedata/walley/", gsub(".*\\/|.sra.fastq", "", files)))
+df$outdir <- as.character(df$outdir)
+for(i in 1:nrow(df)) dir.create(df$outdir[i], showWarnings = FALSE)
 
 
 run_kallisto(df, single=TRUE, l=200, s=20,
